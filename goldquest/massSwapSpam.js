b8467ef38e2977a24eb89902658b8cc9d4aa3c4a
qaiik <82181231+qaiik@users.noreply.github.com>
2022-06-21 15:29:20 -0600

    fix?

--- goldquest/massSwapSpam.js
+++ goldquest/massSwapSpam.js
@@ -14,13 +14,29 @@ function ruinPlayer(name) {
     }, 300)
 }
 
-e.memoizedProps.firebase.getDatabaseVal(e.memoizedProps.client.hostId, "c", (...o) => {
-    let c = Object.keys(...o)
-    c = c.filter(v => {
-        return v !== e.memoizedProps.client.name
+window.blooketBuild = window.webpackJsonp.map(e => Object.keys(e[1]).map(t => e[1][t])).reduce((e, t) => [...e, ...t], []).find(e => /\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/.test(e.toString()) && /\(new TextEncoder\)\.encode\(\"(.+?)\"\)/.test(e.toString())).toString().match(/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/)[0]
+
+window.secret = window.webpackJsonp.map(e => Object.keys(e[1]).map(t => e[1][t])).reduce((e, t) => [...e, ...t], []).find(e => /\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/.test(e.toString()) && /\(new TextEncoder\)\.encode\(\"(.+?)\"\)/.test(e.toString())).toString().match(/\(new TextEncoder\)\.encode\(\"(.+?)\"\)/)[1]
+
+
+(async () => {
+    await fetch("https://fb.blooket.com/c/firebase/join", {
+        headers: {
+            "X-Blooket-Build": window.blooketBuild,
+            "Content-Type": "text/plain"
+        },
+        credentials: "include",
+        method: "put",
+        body: await encodePayload({
+            id: e.memoizedProps.client.hostId,
+            name: 'gtjh45',
+        }, window.secret),
+    }).then(async (res) => {
+        e.memoizedProps.players = (await res.json()).host.c
     })
-    c.forEach((player) => {
-        console.log(`hacking ${player}`)
-        ruinPlayer(player)
+    e.memoizedProps.players.forEach((p) => {
+        console.log(`ruining ${p}`)
+        ruinPlayer(p)
     })
+
 })
