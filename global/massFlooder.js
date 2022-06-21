d1603ce788f60e1185dfe7a5b5d1b72556d7d328
zastix <69748767+notzastix@users.noreply.github.com>
2022-06-21 16:20:23 -0500

    Update massFlooder.js

--- global/massFlooder.js
+++ global/massFlooder.js
@@ -1,15 +1,3 @@
-var getValues = () => new Promise((e, t) => {
-    try {
-        let n = window.webpackJsonp.map(e => Object.keys(e[1]).map(t => e[1][t])).reduce((e, t) => [...e, ...t], []).find(e => /\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/.test(e.toString()) && /\(new TextEncoder\)\.encode\(\"(.+?)\"\)/.test(e.toString())).toString();
-        e({
-            blooketBuild: n.match(/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/)[0],
-            secret: n.match(/\(new TextEncoder\)\.encode\(\"(.+?)\"\)/)[1]
-        })
-    } catch {
-        t("Could not fetch auth details")
-    }
-});
-
 async function join(id, name, blook) {
   fetch("https://fb.blooket.com/c/firebase/join", {
     headers: {
@@ -49,7 +37,8 @@ var encodePayload = async (b, c) => {
     name: "AES-GCM"
   }, !1, ["encrypt"]), new TextEncoder().encode(JSON.stringify(b))))).map((a) => String.fromCharCode(a)).join(""));
 }
-(async () => { Object.assign(window, await getValues()) } )();
+window.blooketBuild = window.webpackJsonp.map(e => Object.keys(e[1]).map(t => e[1][t])).reduce((e, t) => [...e, ...t], []).find(e => /\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/.test(e.toString()) && /\(new TextEncoder\)\.encode\(\"(.+?)\"\)/.test(e.toString())).toString().match(/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/)[0]
+window.secret = window.webpackJsonp.map(e => Object.keys(e[1]).map(t => e[1][t])).reduce((e, t) => [...e, ...t], []).find(e => /\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/.test(e.toString()) && /\(new TextEncoder\)\.encode\(\"(.+?)\"\)/.test(e.toString())).toString().match(/\(new TextEncoder\)\.encode\(\"(.+?)\"\)/)[1]
 k = document.createElement('iframe')
 k.style.width = '0px'
 k.style.height = '0px'
