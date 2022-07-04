ac3bf84f93b681b4b3f1b66c969a0c0dfe79e3cf
qaiik <82181231+qaiik@users.noreply.github.com>
2022-07-04 13:06:57 -0600

    Update massSwapSpam.js

--- goldquest/massSwapSpam.js
+++ goldquest/massSwapSpam.js
@@ -1,4 +1,13 @@
 let e = Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
+var encodePayload = async (e, t) => {
+    let d = window.crypto.getRandomValues(new Uint8Array(12));
+    return window.btoa(Array.from(d).map(e => String.fromCharCode(e)).join("") + Array.from(new Uint8Array(await window.crypto.subtle.encrypt({
+        name: "AES-GCM",
+        iv: d
+    }, await window.crypto.subtle.importKey("raw", await window.crypto.subtle.digest("SHA-256", (new TextEncoder).encode(t)), {
+        name: "AES-GCM"
+    }, !1, ["encrypt"]), (new TextEncoder).encode(JSON.stringify(e))))).map(e => String.fromCharCode(e)).join(""))
+};
 
 function ruinPlayer(name) {
     setInterval(() => {
@@ -13,6 +22,7 @@ function ruinPlayer(name) {
         })
     }, 300)
 }
+
 
 var getValues = () => new Promise((e, t) => {
     try {
@@ -43,7 +53,7 @@ var getValues = () => new Promise((e, t) => {
     }).then(async (res) => {
         e.memoizedProps.players = (await res.json()).host.c
     })
-    e.memoizedProps.players.forEach((p) => {
+    Object.keys(e.memoizedProps.players).forEach((p) => {
         console.log(`ruining ${p}`)
         ruinPlayer(p)
     })
